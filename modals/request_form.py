74255b09d0443b9f50d2d4886fded3a05f79275a
BT-7274-18 <33301661+BT-7274-18@users.noreply.github.com>
2026-05-11 13:18:10 +0000

    requests.py now stores the date when the request was made.

--- modals/request_form.py
+++ modals/request_form.py
@@ -1,4 +1,5 @@
 import discord
+import datetime
 from requests import add_request
 
 
@@ -8,6 +9,7 @@ class RequestForm(discord.ui.Modal, title="Request Form"):
     comment = discord.ui.TextInput(label="Comment", required=False)
 
     async def on_submit(self, interaction: discord.Interaction) -> None:
+        interaction.created_at
         add_request(interaction.user.id, self.mediaTitle.value, self.url.value, self.comment.value)
 
         await interaction.response.send_message("Request submitted.")
--- requests.py
+++ requests.py
@@ -1,9 +1,12 @@
-def get_requests() -> list[list[str|int]]:
+import datetime
+
+
+def get_requests() -> list[list[str|int|datetime.datetime]]:
     """
     Gets all unfullfilled requests from requests file.
 
     Returns:
-        list[list[str]]. List of requests in the format of [[requesterId, mediaTitle, url, comment], ...]
+        list[list]. List of requests in the format of [[requesterId: int, mediaTitle: str, url: str, comment: str, createdOn: datetime.datetime], ...]
     """
     requests = []
 
@@ -19,20 +22,26 @@ def get_requests() -> list[list[str|int]]:
     # end
 
     # convert requesterId to integer
+    # convert created date to datetime object
     for i in range(len(requests)):
         requests[i][0] = int(requests[i][0])
+        requests[i][4] = datetime.datetime.strptime(requests[i][4], "%m-%d-%y")
     # end
 
     return requests
 # end
 
 
-def add_request(requesterId: int, mediaTitle: str, url: str, comment: str):
+def add_request(requesterId: int, mediaTitle: str, url: str, comment: str, createdAt: datetime.datetime):
     """
     Adds the given media request to the requests file.
 
     Arguments:
-        request (list[str]): A media request in the format of [requesterId, mediaTitle, url, comment].
+        requesterId (int): The dicord id of the person that made the request.
+        mediaTitle (str): The title of the media the person wants downloaded.
+        url (str): A url of a webpage with information on the media.
+        comment (str): A user given comment.
+        createdAt (datetime.datetime): A datetime object when the request was created.
     """
 
     # get the current requests
@@ -46,7 +55,7 @@ def add_request(requesterId: int, mediaTitle: str, url: str, comment: str):
     # end
 
     # add the given request to the current requests
-    convertedRequests.append([str(requesterId), mediaTitle, url, comment])
+    convertedRequests.append([str(requesterId), mediaTitle, url, comment, createdAt.strftime("%m-%d-%y")])
 
     # save changes to the requests file
     with open("requests.csv", "w") as writer:
